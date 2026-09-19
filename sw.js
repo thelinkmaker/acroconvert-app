9a59e1465bbe5ea0eb90926eceb46daabd23417a
thelinkmaker <alhanhu@outlook.com>
2026-09-19 13:32:10 +0530

    Update service worker domain and zoneId

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11835691
+    "domain": "3nbf4.com",
+    "zoneId": 11838107
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
